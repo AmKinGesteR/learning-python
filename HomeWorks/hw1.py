d01bd56ee71dbbe569154895a0fab529b85fe238
KinGesteR <139977276+AmKinGesteR@users.noreply.github.com>
2023-07-30 22:07:09 +0330

    Create hw1.py

---
+++ HomeWorks/hw1.py
@@ -0,0 +1,34 @@
+print('\n #------------GETTING TYPES------------# \n')
+
+print(type( 52 ))
+print(type( 98.54526489 ))
+print(type( 'TESTING' ))
+
+
+
+print('\n #-------------RIKHTE GARI-------------# \n')
+
+a = int ( 569 )
+b = float ( 8 )
+c = str ( '4 + 1 - 78 / 4 | Testing...' )
+
+#d = c + b + a        ERROR mide, chon ke yek string ba yek int jam shode (str + float/int = ERROR)
+#print( d )
+
+d = a + b       # javab float mishe, chon ke yek 'int' ba yek 'float' jam shode (int + float = float) (float + float = float) (int + int = int)
+
+print(type( a ))
+print(type( b ))
+print(type( c ))
+print(type( d ))
+
+
+print('\n')
+
+
+print( a )
+print( b )
+print( c )
+print( d )
+
+print('\n #-------------------------------------#')

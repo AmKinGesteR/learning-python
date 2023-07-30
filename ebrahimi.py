e7f8b9eb5cf260e5ca86020d4a6713a1d7b68b71
KinGesteR <139977276+AmKinGesteR@users.noreply.github.com>
2023-07-30 14:13:37 +0330

    Add files via upload

---
+++ ebrahimi.py
@@ -0,0 +1,47 @@
+# --------- moteghayerha --------- #
+
+a = 1   # int
+b = 2   # int
+print(a + b)
+
+c = 1.55     # float
+d = 5.2549   # float
+print(c + d)
+
+e = 'mohammad'   # string (str)
+f = 'hosein'    # string (str)
+print(e + ' ' + f)
+
+
+
+# --------- gereftane type e vorodi ha --------- #
+
+print(type( 2 ))
+print(type( 5.26 ))
+print(type( 'asfasd' ))
+
+
+
+# --------- rikhte gari = force kardane ye chizi ke eshtebahe, yani ini hast ke man migam --------- #
+
+g = int ( 45.2 )  # in dar vaghe 'float' hast, vali man migam ke na, in 'int' hast
+h = float ( 5 )   # in dar vaghe 'int' hast, vali man migam ke na, in 'float' hast
+i = str ( '2' )   # harchi bezani tosh, behet string mide
+
+print(type( g ))
+print(type( h ))
+print(type( i ))
+
+print( g )   # vali chon ke goftam 'int' hast, ashar o hazf kard
+print( h )   # vali chon ke goftam 'float' hast, ashar o ezafe kard
+print( i )   # harchi bezani tosh, behet string mide
+
+
+
+# --------- x --------- #
+
+
+
+
+
+# --------- x --------- #
